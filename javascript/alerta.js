e85126edf24b81bfe7ef588e2db8622844ff4829
Alonso19 <103224841+Auqui19@users.noreply.github.com>
2022-12-15 13:20:30 -0500

    Add files via upload

---
+++ javascript/alerta.js
@@ -0,0 +1,83 @@
+//
+function alertabtn() {
+  Swal.fire({
+    imageUrl: "img/cara-triste.png",
+    imageWidth: "150px",
+    title: '<h4 class="color">PROYECTO POR TERMINAR</h4>',
+    html: '<p class="color">Este proyecto falta por terminar, una vez terminado se enviará una notificación</p>',
+
+    confirmButtonText: '<p class="color-black">OK</p>',
+    background: "#090b0d",
+    confirmButtonColor: "#41f2c1",
+  });
+}
+function alertadc() {
+  Swal.fire({
+    imageUrl: "img/discord-logo.png",
+    imageWidth: "100px",
+    title: '<h4 class="color">AGREGAME EN DISCORD</h4>',
+    html: '<p class="color">IAlonso#0800</p>',
+    confirmButtonText: '<p class="color-black">OK</p>',
+    background: "#090b0d",
+    confirmButtonColor: "#41f2c1",
+  });
+}
+//Swal.fire({
+// title:
+// text:
+//html: '<b>⚡ CoderPromo ⚡ Aprovecha 50% OFF en nuestros cursos ⏰ Último día ⏰</b>',
+// icon:
+//confirmButtonText: 'ok',
+// footer:
+//width: '33%',
+//padding: '5px',
+// background:
+//grow: false,
+//backdrop: false,
+//timer: '10000',
+//timerProgressBar: true,
+//toast: true,
+//position: 'bottom-end',
+//allowOutsideClick: false,
+//allowEscapeKey: false,
+// allowEnterKey:
+//stopKeydownPropagation: false,
+
+// input:
+// inputPlaceholder:
+// inputValue:
+// inputOptions:
+
+//  customClass:
+// 	container:
+// 	popup:
+// 	header:
+// 	title:
+// 	closeButton:
+// 	icon:
+// 	image:
+// 	content:
+// 	input:
+// 	actions:
+// 	confirmButton:
+// 	cancelButton:
+// 	footer:
+
+//showConfirmButton: false,
+// confirmButtonColor:
+// confirmButtonAriaLabel:
+
+//showCancelButton: false,
+// cancelButtonText:
+// cancelButtonColor:
+// cancelButtonAriaLabel:
+
+// buttonsStyling:
+// showCloseButton:
+// closeButtonAriaLabel:
+
+// imageUrl:
+// imageWidth:
+// imageHeight:
+// imageAlt:
+//});
---
+++ javascript/varscroll.js
@@ -0,0 +1,88 @@
+document.querySelector(".menu-btn").addEventListener("click", animacionbar);
+
+var barradespegable = document.querySelector(".nav-list");
+var barramenu1 = document.querySelector(".barra-btn1");
+var barramenu2 = document.querySelector(".barra-btn2");
+var barramenu3 = document.querySelector(".barra-btn3");
+
+function animacionbar() {
+  barradespegable.classList.toggle("show");
+  barramenu1.classList.toggle("activebarra-btn1");
+  barramenu2.classList.toggle("activebarra-btn2");
+  barramenu3.classList.toggle("activebarra-btn3");
+}
+
+//SCROLL
+var lastScrollTop = 0;
+navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-80px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
+
+//ScrollReveal
+window.sr = ScrollReveal();
+
+sr.reveal(".header-item h2", {
+  duration: 1000,
+  origin: "left",
+  distance: "50px",
+});
+
+sr.reveal(".header-item h1", {
+  duration: 2000,
+  origin: "left",
+  distance: "50px",
+});
+
+sr.reveal(".header-item h3", {
+  duration: 3000,
+  origin: "left",
+  distance: "50px",
+});
+
+sr.reveal(".text-mi", {
+  duration: 3000,
+  origin: "left",
+  distance: "100px",
+});
+
+sr.reveal(".picture-img", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "40px",
+});
+
+const typed = new Typed(".typed", {
+  strings: ["soy programador web", "creo proyectos webs"],
+  typeSpeed: 75,
+  starDelay: 300,
+  backSpeed: 75,
+  smartBackspace: true,
+  shuffle: false,
+  backDelay: 1500,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: "|",
+  contentType: "html",
+});
+
+let mostrar_cajas_btn = document.getElementById("mostrar-cajas-btn");
+let mostrar_cajas = document.getElementById("mostrar-cajas");
+
+mostrar_cajas_btn.addEventListener("click", toggleBox);
+function toggleBox() {
+  mostrar_cajas.classList.toggle("mostrar");
+
+  if (mostrar_cajas.classList.contains("mostrar")) {
+    mostrar_cajas_btn.innerHTML = "Ver Menos";
+  } else {
+    mostrar_cajas_btn.innerHTML = "Ver Más";
+  }
+}
